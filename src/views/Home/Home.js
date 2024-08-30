f7cc220589cbcb3bc65042769e6da341fdbd1950
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2024-08-30 20:40:09 +0530

    add features

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,29 +3,73 @@ import './Home.css'
 import note from './noties.png'
 import pen from './quill-pen.png'
 import book from './book.png'
+import privite from "./privacy-settings.png"
+import easy from "./correct.png"
+import like from "./heart.png"
+import Featurescard from "../../components/Featurescard/Featurescard"
+
+const FEATURES = [
+  {
+    img: privite,
+    info: 'Note Safe'
+  },
+  {
+    img: easy,
+    info: "Easy to Us"
+  },
+  {
+    img: like,
+    info: "100K"
+  }
+]
+
 
 function Home() {
   return (
+
     <div>
 
       <div className='hedding-continer'>
 
         <h1 className='text-center web-title'> <img src={note} className='img' /> Noties !</h1>
-        <h6 className='text-center title-info'> Your friendly reminder app. {" "} 
+        <h6 className='text-center title-info'> Your friendly reminder app. {" "}
           <del>Quick notes for a busy life! </del>{" "}  <span className='hight-light'>Your friendly reminder app 📘 </span></h6>
       </div>
 
       <div className='btn-continer'>
-        
-        
-        
-        <button className='btn btn-fist'>  Add Note  <img src= {pen} className='logo'/></button>
-        <button className='btn  btn-second'>  Show   <img src= {book} className='logo'/></button>
+
+
+
+        <button className='btn btn-fist'>  Add Note  <img src={pen} className='logo' /></button>
+        <button className='btn  btn-second'>  Show   <img src={book} className='logo' /></button>
+
+
+      </div>
+      <div className='features-continer'>
+
+        {
+          FEATURES.map(fetures => {
+            return (
+              <Featurescard
+                img={fetures.img}
+                info={fetures.info}
+
+              />
+
+            )
+          })
+
+        }
+
+
+
 
 
       </div>
 
     </div>
+    
+
   )
 }
 
